327349d01f27b45c0c74c25212b55321828fa290
Sam Barati <sambb@kth.se>
2023-01-04 17:25:16 +0100

    Initial functionality.

---
+++ mixtape_maker/pages/login/index.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+
+const AUTH_URL =
+  "https://accounts.spotify.com/authorize?client_id=&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20playlist-read-private%20ugc-image-upload%20playlist-modify-private%20playlist-modify-public%20user-read-email";
+
+export default function Login() {
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <a className="btn btn-success btn-lg" href={AUTH_URL}>
+        Click Me!
+      </a>
+    </Container>
+  );
+}
