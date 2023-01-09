6a1f4613fe79b447e3ffad64de5a84368eea4c51
Sam Barati <sambb@kth.se>
2023-01-09 23:15:15 +0100

    Better looking profile view, redirect when pressing playlist

--- mixtape_maker/pages/profile/index.js
+++ mixtape_maker/pages/profile/index.js
@@ -10,46 +10,60 @@ export default function Profile() {
   const { data: session } = useSession();
   const [playlists, setPlaylists] = useState(null);
 
-  useEffect(()=>{
-    userPlaylistsToDb(playlists, session?.user.sub)
-  }, [playlists])
+  useEffect(() => {
+    userPlaylistsToDb(playlists, session?.user.sub);
+  }, [playlists]);
 
   useEffect(() => {
     if (spotifyApi.getAccessToken()) {
       spotifyApi
         .getUserPlaylists()
-        .then((data) => setPlaylists(data.body.items))
-        ;
+        .then((data) => setPlaylists(data.body.items));
     }
   }, [session, spotifyApi]);
 
   return (
-    <Container>
-      <h1 className="text-center">Welcome, {session?.user.name}!</h1>
-      <h2 className="text-center">Your current playlists: </h2>
-      <table>
-        {playlists?.map((playlist) => {
-          return (
-            <tr>
-              <td key={playlist.id}>
-                <p>{playlist.name}</p>
-              </td>
-            </tr>
-          );
-        })}
-      </table>
+    <>
       <Container
-        className="d-flex justify-content-center align-items-center"
+        className="justify-content-center align-items-center"
         style={{ minHeight: "100vh" }}
       >
-        <Button
-          onClick={() => {
-            signOut({ callbackUrl: "/" });
-          }}
-        >
-          Sign Out!
-        </Button>
+        <h1 className="text-center ">Welcome, {session?.user.name}!</h1>
+        <h2 className="text-center">Your current playlists: </h2>
+        <div className="container d-flex gap-2">
+          {playlists?.map((playlist) => {
+            return (
+              <div className="row">
+                <div className="col-sm">
+                  <a href={ session.user.accessToken ? playlist.external_urls.spotify:""}>
+                    <img
+                      src={playlist.images[0].url}
+                      width="200"
+                      height="200"
+                    ></img>
+                  </a>
+                </div>
+                <div className="w-100"></div>
+                <div className="col-sm">
+                  <p>{playlist.name}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </Container>
-    </Container>
+      <div className="container">
+        <div className="col-md-12 text-center position-absolute top-50 start-0 translate-middle-y">
+          <Button
+            onClick={() => {
+              signOut({ callbackUrl: "/" });
+            }}
+            className=" justify-content-center align-items-center"
+          >
+            Sign Out!
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
