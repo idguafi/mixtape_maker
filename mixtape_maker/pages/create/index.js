3cca04543a2ed7d860da28a52c7401b0cb29b8d0
Sam Barati <sambb@kth.se>
2023-01-18 10:24:09 +0100

    Search for song and artist with form

--- mixtape_maker/pages/create/index.js
+++ mixtape_maker/pages/create/index.js
@@ -7,17 +7,23 @@ export default function Create() {
   const [tracks, setTracks] = useState([]);
   const spotifyApi = useSpotify();
   const { data: session } = useSession();
+  const [artistFieldState, setArtistField] = useState("");
+  const [titleFieldState, setTitleField] = useState("");
 
-  useEffect(() => {
+  function search(){
+    const payload = JSON.stringify({artist: artistFieldState, title:titleFieldState})
+    setArtistField("")
+    setTitleField("")
+
     if (spotifyApi.getAccessToken()) {
-      spotifyApi.searchTracks("track:Alright artist:Kendrick Lamar").then(
+      spotifyApi.searchTracks(payload).then(
         function (data) {
           if (
             tracks.some((track) => track.id === data.body.tracks.items[0].id)
           ) {
-            console.log("hi");
             return;
           } else {
+            data.body.tracks.items.map((track)=>{track.name.toLowerCase().includes(payload.title?.toLowerCase()) ? console.log(track):console.log("no")})
             setTracks([...tracks, data.body.tracks.items[0]]);
           }
         },
@@ -26,7 +32,17 @@ export default function Create() {
         }
       );
     }
-  }, [session, spotifyApi]);
+  }
 
-  return <CreateView tracks={tracks} />;
+
+  return (
+    <CreateView
+      tracks={tracks}
+      search={search}
+      titleFieldState={titleFieldState}
+      artistFieldState={artistFieldState}
+      setArtistField={(data) => setArtistField(data.target.value)}
+      setTitleField={(data) => setTitleField(data.target.value)}
+    />
+  );
 }
--- mixtape_maker/src/views/create.js
+++ mixtape_maker/src/views/create.js
@@ -1,37 +1,55 @@
 export default function CreateView(props) {
-
   function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
     var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
   }
 
   return (
-    <div className="container d-flex align-items-center min-vh-50">
-      <div className="table text-center">
-        <table className="table table-sm">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Artist</th>
-              <th scope="col">Title</th>
-              <th scope="col">Duration</th>
-            </tr>
-          </thead>
-          {props.tracks?.map((track, index) => {
-            return (
-              <tbody>
-                <tr>
-                  <th scope="row">{index + 1}</th>
-                  <td>{track.artists[0].name}</td>
-                  <td>{track.name}</td>
-                  <td>{millisToMinutesAndSeconds(track.duration_ms)}</td>
-                </tr>
-              </tbody>
-            );
-          })}
-        </table>
+    <>
+      <div className="container text-center input-group w-50">
+        <input
+          class="form-control"
+          placeholder="artist name:"
+          value={props.artistFieldState}
+          onChange={props.setArtistField}
+        ></input>
+        <input
+          class="form-control"
+          placeholder="song title:"
+          value={props.titleFieldState}
+          onChange={props.setTitleField}
+        ></input>
+        <button className="search" onClick={props.search}>
+          search
+        </button>
       </div>
-    </div>
+      <div className="container p-5 my-5 d-flex align-items-center min-vh-50">
+        <div className="table text-center">
+          <table className="table table-sm">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Artist</th>
+                <th scope="col">Title</th>
+                <th scope="col">Duration</th>
+              </tr>
+            </thead>
+            {props.tracks?.map((track, index) => {
+              return (
+                <tbody>
+                  <tr>
+                    <th scope="row">{index + 1}</th>
+                    <td>{track.artists[0].name}</td>
+                    <td>{track.name}</td>
+                    <td>{millisToMinutesAndSeconds(track.duration_ms)}</td>
+                  </tr>
+                </tbody>
+              );
+            })}
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
