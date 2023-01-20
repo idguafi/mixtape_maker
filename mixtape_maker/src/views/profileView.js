53b17cd36fb69b7abbe7bbc2b1a5a2e58b8d215f
Sam Barati <sambb@kth.se>
2023-01-20 11:00:16 +0100

    profile page responsive

--- mixtape_maker/src/views/profileView.js
+++ mixtape_maker/src/views/profileView.js
@@ -4,42 +4,30 @@ import { signOut } from "next-auth/react";
 export default function ProfileView(props) {
   return (
     <>
-      <Container
-        className="justify-content-center align-items-center"
-        style={{ minHeight: "50vh" }}
-      >
+      <div className="container">
         <br></br>
         <h1 className="text-center">YOUR_MXTPS:</h1>
         <br></br>
-        <div className="container d-flex gap-2">
-
+        <div className="row  d-flex justify-content-around">
           {props.playlists?.map((playlist) => {
             return (
-              <div className="row">
-                <div className="col-sm">
-                  <a
-                    href={
-                      props.user.accessToken
-                        ? playlist.external_urls.spotify
-                        : ""
-                    }
-                  >
-                    <img
-                      src={playlist.images[0].url}
-                      width="200"
-                      height="200"
-                    ></img>
-                  </a>
-                </div>
-                <div className="w-100"></div>
-                <div className="col-sm">
-                  <p>{playlist.name}</p>
-                </div>
+              <div className="col-md-4">
+                <a
+                  href={
+                    props.user.accessToken ? playlist.external_urls.spotify : ""
+                  }
+                >
+                  <img
+                    src={playlist.images[0].url}
+                    class="mx-auto d-block img-fluid"
+                  ></img>
+                </a>
+                <p>{playlist.name}</p>
               </div>
             );
           })}
         </div>
-      </Container>
+      </div>
     </>
   );
 }
