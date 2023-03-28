34f834abdb75f03b6e51db11fcb328726fbfc444
Sam Barati <sambb@kth.se>
2023-03-28 20:52:53 +0200

    updated firestoreConfig

--- mixtape_maker/lib/firestoreConfig.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBA21Gu-h490Bi-kkH15Uwi6jQbdjN2zyU",
-    authDomain: "mixtape-maker-66ded.firebaseapp.com",
-    projectId: "mixtape-maker-66ded",
-    storageBucket: "mixtape-maker-66ded.appspot.com",
-    messagingSenderId: "177258425038",
-    appId: "1:177258425038:web:a793d4241918ade9547e9f",
-  };
--- mixtape_maker/lib/firestoredb.js
+++ mixtape_maker/lib/firestoredb.js
@@ -14,7 +14,14 @@ import {
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { firebaseConfig } from "./firestoreConfig";
+const firebaseConfig = {
+  apiKey: "AIzaSyBA21Gu-h490Bi-kkH15Uwi6jQbdjN2zyU",
+  authDomain: "mixtape-maker-66ded.firebaseapp.com",
+  projectId: "mixtape-maker-66ded",
+  storageBucket: "mixtape-maker-66ded.appspot.com",
+  messagingSenderId: "177258425038",
+  appId: "1:177258425038:web:a793d4241918ade9547e9f",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
