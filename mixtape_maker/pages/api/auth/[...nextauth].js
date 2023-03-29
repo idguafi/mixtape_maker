cd774a71078212773a2803422fb4664db0e8d60e
Sam Barati <sambb@kth.se>
2023-03-29 09:23:28 +0200

    updated nextAuth

--- mixtape_maker/pages/api/auth/[...nextauth].js
+++ mixtape_maker/pages/api/auth/[...nextauth].js
@@ -48,13 +48,13 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     SpotifyProvider({
-      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_ID,
+      clientSecret: process.env.NEXT_PUBLIC_SECRET,
       authorization: LOGIN_URL,
     }),
     // ...add more providers here
   ],
-  secret: process.env.JWT_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 
   adapter: FirestoreAdapter(firebaseConfig),
 
